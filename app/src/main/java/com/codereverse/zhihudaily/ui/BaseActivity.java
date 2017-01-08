2aba767c54cca0094e5d0715843d6e42436144fb
Reversal <18918486029@163.com>
2017-01-08 23:53:37 +0800

    init

---
+++ app/src/main/java/com/codereverse/zhihudaily/ui/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.codereverse.zhihudaily.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Dennis Qin on 2017/1/8.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
